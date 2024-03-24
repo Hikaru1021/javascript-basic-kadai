f11dbe761f78ecf4ba9f4c6a2898193ad149a84b
Hikaru Yoshiki <kagayaki.kirari.1021@gmail.com>
2024-03-24 20:01:36 +0900

    Merge branch 'main' of https://github.com/Hikaru1021/javascript-basic-kadai

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
     text.textContent = 'クリックしました';
 })
-=======
-
- const text = document.getElementById('text');
-
- btn.addEventListener('click', () => {
-    text.textContent = 'クリックしました';
- })
->>>>>>> 652afe8fb84da99b86e164ba20dcbf0ce28fd2b9
